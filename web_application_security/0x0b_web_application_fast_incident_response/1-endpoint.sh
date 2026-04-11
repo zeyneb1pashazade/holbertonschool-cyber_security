c881279fca8149bdd4432dac6b3d77bb2970b860
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-04-11 07:14:47 -0400

    endpoint

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $7}' logs.txt | sort | uniq -c | sort -nr | tr  -s ' '| cut -d ' ' -f3
+awk '{print $7}' logs.txt | sort | uniq -c | sort -nr |head -n 1| tr  -s ' '| cut -d ' ' -f3
