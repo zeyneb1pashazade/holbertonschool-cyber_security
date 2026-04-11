f2d278f6b012bc08b464815e62d806fd9e907565
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-04-11 07:05:53 -0400

    dos attack

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1| awk '{print #2}'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1| tr -s ' ' | cut -d' ' -f3
