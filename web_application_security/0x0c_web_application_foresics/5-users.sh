31f53d897b320408e995fef865d19f77e75a169a
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-04-17 09:16:40 -0400

    logs-3

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Ei "adduser|useradd" auth.log* |awk -F "name=" '{print $2}'| awk '{print $1}'|tr -d '\n' | sed 's/,$//'
+grep -Ei "adduser|useradd" auth.log* |awk -F "name=" '{print $2}'| awk '{print $1}'|sort|tr -d '\n' | sed 's/,$//'
