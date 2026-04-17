8edbcc7c99e814c6ea1458e67259101d39968efb
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-04-17 08:02:30 -0400

    logs-2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log |grep -E "Failed password|Accepted password|session opened" | awk '{print $1, $2, $3, $9}' | uniq -c  
+grep "Accepted password" auth.log | awk '{print $9}' | head -n 1
