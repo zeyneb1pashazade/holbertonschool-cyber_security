3db1386c9a638163c620f9f3004d799fb90d4fb6
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-04-23 09:31:32 -0400

    nmap5

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -O -sV --traceroute -sC --script=banner,ssl-enum-ciphers -oN service_enumeration_results.txt "$1" 
+nmap -O -sV -A --traceroute --script=banner,ssl-enum-ciphers,default,smb-enum-domains -oN service_enumeration_results.txt "$1" 
