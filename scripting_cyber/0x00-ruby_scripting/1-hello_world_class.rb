9a651faa268e98f8b0795ae5331f0e115f67d91b
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2026-05-21 07:35:28 -0400

    ruby1

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,9 @@
+class HelloWorld
+  def initialize
+    @message = "Hello, World!"
+  end
+
+  def print_hello
+    puts @message
+  end
+end
---
+++ scripting_cyber/0x00-ruby_scripting/1-main.rb
@@ -0,0 +1,6 @@
+require_relative '1-hello_world_class'
+
+# Create an instance of HelloWorld, change the message, and call the print_hello method
+hello_world_instance = HelloWorld.new
+
+hello_world_instance.print_hello
